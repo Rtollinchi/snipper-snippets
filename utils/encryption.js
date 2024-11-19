7d0da478b753f107e3a72c9b1aab9bf2e8bc9db2
Rtollinchi <rtollinchi23@gmail.com>
2024-11-19 17:35:18 -0500

    add encrypt and decrypt functions along with creating .env file with ENCRYPTION_KEY and installing npm i dotenv

---
+++ utils/encryption.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const crypto = require("crypto");
+
+const key = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
+
+const algorithm = "aes-256-cbc";
+
+function encrypt(plainText) {
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  let encryptedText = cipher.update(plainText, "utf8", "hex");
+  encryptedText += cipher.final("hex");
+
+  return iv.toString("hex") + ":" + encryptedText;
+}
+
+function decrypt(input) {
+  const parts = input.split(":");
+  const iv = Buffer.from(parts[0], "hex");
+  const encryptedText = parts[1];
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  let decryptedText = decipher.update(encryptedText, "hex", "utf8");
+  decryptedText += decipher.final("utf8");
+
+  return decryptedText;
+}
+
+// const plainText = "Hello, World!";
+// const encryptedText = encrypt(plainText);
+// console.log("Encrypted Text:", encryptedText);
+
+// const decryptedText = decrypt(encryptedText);
+// console.log("Decrypted Text:", decryptedText);
+
+module.exports = { encrypt, decrypt };
